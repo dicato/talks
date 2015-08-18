d839fbc54ad9606c9820d197b6734701fc29375a
Patrick Cloke <clokep@patrick.cloke.us>
2015-08-18 14:26:50 -0400

    Hook up the API to the chat server.

--- boston_python_08_27_2015/catchat/api.py
+++ boston_python_08_27_2015/catchat/api.py
@@ -1,32 +1,27 @@
 import json
 
-from twisted.internet import reactor, task, endpoints
+from twisted.internet import reactor, task
 from twisted.python import log
-from twisted.web import server, resource
+from twisted.web import resource, server
 
 
-BANNER = 'None!'
+class ApiResource(resource.Resource):
+    # Note that this is available as self.server for every resource.
 
-def get_banner():
-    return "MESSAGE OF THE DAY:\n\n + %s\n\n" % BANNER
+    def __init__(self, chat_factory, *args, **kwargs):
+        # This needs a reference to the NetCatChat factory object.
+        self.chat_factory = chat_factory
 
-
-def set_banner(message):
-    global BANNER
-    BANNER = message
-
-
-def get_user_count():
-    return 7  # always 7!!
+        resource.Resource.__init__(self, *args, **kwargs)
 
 
-class Root(resource.Resource):
+class Root(ApiResource):
     isLeaf = False
 
-    def __init__(self):
-        resource.Resource.__init__(self)
-        self.putChild('banner', Banner())
-        self.putChild('users', Users())
+    def __init__(self, *args, **kwargs):
+        ApiResource.__init__(self, *args, **kwargs)
+        self.putChild('banner', Banner(self.chat_factory))
+        self.putChild('users', Users(self.chat_factory))
 
     def getChild(self, name, request):
         if name == "":
@@ -41,30 +36,37 @@ class Root(resource.Resource):
         '''
 
 
-class Banner(resource.Resource):
+class Banner(ApiResource):
     isLeaf = True
 
+    def _set_banner(self, banner):
+        # The json modules returns unicode, we need bytes.
+        banner = bytes(banner)
+        # Ensure there's a line break at the end.
+        if not banner.endswith(b'\r\n'):
+            banner += b'\r\n'
+        self.chat_factory.banner = banner
+
     def render_GET(self, request):
-        # TODO: Replace get_user_count with actual user count from server.
-        result = {'banner': get_banner()}
+        # Get the current banner from the server.
+        result = {'banner': self.chat_factory.banner}
 
         return json.dumps(result, indent=4, separators=(',', ': ')) + "\n"
 
     def render_POST(self, request):
         status = "ERROR"
         try:
-            data = json.loads(request.content.read())['banner']
+            content = request.content.read()
+            data = json.loads(content)['banner']
 
             # Make this a Deferred so the function can immediately return.
-            # TODO: Replace print_banner with something that actually sets
-            # the banner in the running server.
-            d = task.deferLater(reactor, 0.1, set_banner, data)
+            d = task.deferLater(reactor, 0.1, self._set_banner, data)
             d.addErrback(log.err)
 
             status = "SUCCESS"
         except ValueError:
             # TODO
-            log.err("Unable to parse JSON: %s" % request.content.read())
+            log.err("Unable to parse JSON: %s" % content)
         except KeyError as e:
             # TODO
             log.err("Data does not contain %s" % e.message)
@@ -74,17 +76,12 @@ class Banner(resource.Resource):
         return json.dumps(result)
 
 
-class Users(resource.Resource):
+class Users(ApiResource):
     isLeaf = True
 
     def render_GET(self, request):
-        # TODO: Replace get_user_count with actual user count from server.
-        result = {'users': get_user_count()}
+        # The user count from server.
+        user_count = len(self.chat_factory.clients)
+        result = {'users': user_count}
 
         return json.dumps(result, indent=4, separators=(',', ': ')) + "\n"
-
-site = server.Site(Root())
-
-if __name__ == "__main__":
-    endpoints.serverFromString(reactor, "tcp:8080").listen(site)
-    reactor.run()
--- boston_python_08_27_2015/catchat/runner.py
+++ boston_python_08_27_2015/catchat/runner.py
@@ -1,15 +1,16 @@
 from twisted.internet import reactor, endpoints
+from twisted.web import server
 
 from chat import NetCatChatFactory
-#from api import ApiFactory
+from api import Root
 
 # Create an instance of the factories.
 factory = NetCatChatFactory()
-#api_factory = ApiFactory()
+site = server.Site(Root(factory))
 
 # Listen on TCP port 1400 for chat and port 80 for the API.
 endpoints.serverFromString(reactor, "tcp:1400").listen(factory)
-#endpoints.serverFromString(reactor, "tcp:80").listen(api_factory)
+endpoints.serverFromString(reactor, "tcp:8080").listen(site)
 
 # Start listening for connections.
 reactor.run()
