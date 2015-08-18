e351e59befa16177dc5e0d5bf6992e560199c9fe
Stephen DiCato <stephendicato@gmail.com>
2015-08-18 15:23:10 -0400

    Add ability to set banner from dashboard.

--- boston_python_08_27_2015/catchat/dashboard.py
+++ boston_python_08_27_2015/catchat/dashboard.py
@@ -29,6 +29,14 @@ def get_banner():
     return result['banner']
 
 
+def set_banner(banner):
+    url = API_URL + '/banner/'
+    data = json.dumps({'banner': banner})
+    try:
+        r = requests.post(url, data=data)
+    except Exception as e:  # Gotta catch 'em all!
+        return None
+
 @app.route("/")
 def splash():
     user_count = get_user_count()
@@ -36,9 +44,10 @@ def splash():
     return render_template('index.html', user_count=user_count, banner=banner)
 
 
-@app.route('/set_banner', methods=['POST'])
-def set_banner():
+@app.route('/set_banner/', methods=['POST'])
+def banner():
     banner = request.values.get('banner', "")
+    set_banner(banner)
     return redirect(url_for('splash'))
 
 
