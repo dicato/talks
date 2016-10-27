89b78777445dbdc498cb6521205bc98fc36704e9
Patrick Cloke <clokep@patrick.cloke.us>
2016-10-27 10:52:09 -0400

    Change the syntax highlight theme.

--- django_boston_10_27_2016/conf.py
+++ django_boston_10_27_2016/conf.py
@@ -108,7 +108,7 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', 'README.rst']
 # show_authors = False
 
 # The name of the Pygments (syntax highlighting) style to use.
-pygments_style = 'sphinx'
+pygments_style = 'default'
 
 # A list of ignored prefixes for module index sorting.
 # modindex_common_prefix = []
